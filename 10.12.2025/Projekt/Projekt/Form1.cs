342bd008f97c02e9e2a1ef486e530807da2cd75b
fedesiak <120318227+fedesiak@users.noreply.github.com>
2025-12-10 12:35:51 +0100

    Add files via upload

---
+++ 10.12.2025/Projekt/Projekt/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kołoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+
+            textBox1.Visible = true;
+            textBox2.Visible = false;
+            label2.Visible = false;
+            textBox3.Visible = true;
+            button1.Visible = true;
+            label3.Visible = true;
+            label1.Text = "promień: ";
+            label3.Text = "Pole Koła";
+            
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") 
+            {
+                if (label3.Text == "Pole Koła")
+                {
+                    float a = float.Parse(textBox1.Text);
+                    // float b = float.Parse(textBox2.Text);
+                    float w = 0;
+                    float pi = 3.14f;
+                    w = pi * (a * a);
+                    textBox3.Text = w.ToString();
+                }
+                
+                 else if (label3.Text == "Pole Prostokątu")
+                    {
+                        float a = float.Parse(textBox1.Text);
+                    if (textBox2.Text != "")
+                    {
+
+                        float b = float.Parse(textBox2.Text);
+                        float w = 0;
+                        w = a * b;
+                        textBox3.Text = w.ToString();
+
+                    }
+                    else 
+                    {
+                        textBox3.Text = "podaj wartosc 2";
+                    }
+
+                        // float b = float.Parse(textBox2.Text);
+                        
+                    }
+                
+               
+
+                else if (label3.Text == "Obwód Kwadratu")
+                {
+                    float a = float.Parse(textBox1.Text);
+                    // float b = float.Parse(textBox2.Text);
+                    float w = 0;
+                    w = a * 4;
+                    textBox3.Text = w.ToString();
+                }
+
+                else if (label3.Text == "Obwód Koła")
+                {
+                    float a = float.Parse(textBox1.Text);
+                    // float b = float.Parse(textBox2.Text);
+                    float w = 0;
+                    float pi = 3.14f;
+                    w = 2 * pi * a;
+                    textBox3.Text = w.ToString();
+                }
+               
+                else if (label3.Text == "Objętość Walca")
+                {
+                    float pi = 3.14f, w = 0;
+                    float r = float.Parse(textBox1.Text);
+                    if (textBox2.Text != "")
+                    {
+                        float h = float.Parse(textBox2.Text);
+                        w = pi * (r * r) * h;
+                        textBox3.Text = w.ToString();
+                    }
+                    else
+                    {
+                        textBox3.Text = "podaj wartosc 2";
+                    }
+
+                    // float b = float.Parse(textBox2.Text);
+
+                }
+                
+                
+         
+                else if (label3.Text == "Objętość Stożka")
+                {
+                    float pi = 3.14f, w = 0;
+
+                    float r = float.Parse(textBox1.Text);
+                    if (textBox2.Text != "")
+                    {
+                        float h = float.Parse(textBox2.Text);
+                    w = (h / 3) * pi * (r * r);
+                    textBox3.Text = w.ToString();
+                    }
+                    else
+                    {
+                        textBox3.Text = "podaj wartosc 2";
+                    }
+                }
+                else if (label3.Text == "Pole Powierzchni Kuli")
+                {
+                    float pi = 3.14f, w = 0;
+                    float r = float.Parse(textBox1.Text);
+                    w = 4 * pi * (r * r);
+                    textBox3.Text = w.ToString();
+                }
+            }
+            else
+            {
+                textBox3.Text = "Podaj wartosc 1";
+            }
+           
+
+
+        }
+
+        private void ątToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            button1.Visible = true;
+            label3.Visible = true;
+            label1.Text = "1 bok: ";
+            label2.Text = "2 bok: ";
+            label3.Text = "Pole Prostokątu";
+        }
+
+        private void kwadratToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+ 
+            textBox1.Visible = true;
+            textBox2.Visible=false;
+            label2.Visible=false;
+
+            textBox3.Visible = true;
+            button1.Visible = true;
+            label3.Visible = true;
+            label1.Text = "bok: ";
+            label3.Text = "Obwód Kwadratu";
+        }
+
+        private void kołoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+ 
+            textBox1.Visible = true;
+            textBox2.Visible = false;
+            label2.Visible = false;
+            textBox3.Visible = true;
+            button1.Visible = true;
+            label3.Visible = true;
+            label1.Text = "promień: ";
+            label3.Text = "Obwód Koła";
+        }
+
+        private void walecToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //π r² h
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox3.Visible = true;
+            button1.Visible = true;
+            label3.Visible = true;
+            label1.Text = "promień: ";
+            label2.Text = "wysokość: ";
+            label3.Text = "Objętość Walca";
+        }
+
+        private void stożekToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox3.Visible = true;
+            button1.Visible = true;
+            label3.Visible = true;
+            label1.Text = "promień: ";
+            label2.Text = "wysokość: ";
+            label3.Text = "Objętość Stożka";
+        }
+
+        private void kulaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+
+            label1.Visible = true;
+
+            textBox3.Visible = true;
+            button1.Visible = true;
+            label3.Visible = true;
+            label1.Text = "promień: ";
+            label3.Text = "Pole Powierzchni Kuli";
+        }
+    }
+}
---
+++ 10.12.2025/egzamin 27.11/egzamin 2023-01-01/egzamin 2023-01-01/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace egzamin_2023_01_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ileZnakow = int.Parse(textBox3.Text);
+
+            char[] tabMale = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z' };
+            char[] tabDuze = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] tabCyfry = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] tabZnakiSpecjalne = { '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '{', '|', '}', '~' };
+            /*char[] tabMaleDuze = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] tab2MaleDuzeCyfry = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] tab2CyfryZnaki = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '{', '|', '}', '~' };
+            char[] tab2MaleDuzeZnaki = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '{', '|', '}', '~' };
+            char[] tab3MaleDuzeCyfryZnaki = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '{', '|', '}', '~' };
+            */
+            string haslo = "";
+            Random rand = new Random();
+            int m = 0,n,p,q;
+      
+               if(checkBox1.Checked && checkBox2.Checked && checkBox3.Checked)
+                {
+                    m = rand.Next(0, tabDuze.Length);
+                    haslo += tabDuze[m];
+                    n = rand.Next(0, tabCyfry.Length);
+                    haslo += tabCyfry[n];
+                    p = rand.Next(0,tabZnakiSpecjalne.Length);
+                    haslo += tabZnakiSpecjalne[p];
+                    for(int j=0; j<ileZnakow-3; j++)
+                    {
+                        q = rand.Next(0, tabMale.Length);
+                        haslo += tabMale[q];
+                       
+                    }
+
+                }
+                else if(checkBox1.Checked && checkBox2.Checked)
+                {
+                    m = rand.Next(0, tabDuze.Length);
+                    haslo += tabDuze[m];
+                    n = rand.Next(0, tabCyfry.Length);
+                    haslo += tabCyfry[n];
+                    for (int j = 0; j < ileZnakow - 2; j++)
+                    {
+                        q = rand.Next(0, tabMale.Length);
+                        haslo += tabMale[q];
+
+                    }
+                }
+                else if(checkBox1.Checked && checkBox3.Checked)
+                {
+                    m = rand.Next(0, tabDuze.Length);
+                    haslo += tabDuze[m];
+                    p = rand.Next(0, tabZnakiSpecjalne.Length);
+                    haslo += tabZnakiSpecjalne[p];
+                    for (int j = 0; j < ileZnakow - 2; j++)
+                    {
+                        q = rand.Next(0, tabMale.Length);
+                        haslo += tabMale[q];
+
+                    }
+                }
+                else if(checkBox2.Checked && checkBox3.Checked)
+                {
+                    n = rand.Next(0, tabCyfry.Length);
+                    haslo += tabCyfry[n];
+                    p = rand.Next(0, tabZnakiSpecjalne.Length);
+                    haslo += tabZnakiSpecjalne[p];
+                    for (int j = 0; j < ileZnakow - 2; j++)
+                    {
+                        q = rand.Next(0, tabMale.Length);
+                        haslo += tabMale[q];
+
+                    }
+                }
+                else if(checkBox1.Checked)
+                {
+                    m = rand.Next(0, tabDuze.Length);
+                    haslo += tabDuze[m];
+                    for (int j = 0; j < ileZnakow - 1; j++)
+                    {
+                        q = rand.Next(0, tabMale.Length);
+                        haslo += tabMale[q];
+
+                    }
+                }
+                else if (checkBox2.Checked)
+                {
+                    n = rand.Next(0, tabCyfry.Length);
+                    haslo += tabCyfry[n];
+                    for (int j = 0; j < ileZnakow - 1; j++)
+                    {
+                        q = rand.Next(0, tabMale.Length);
+                        haslo += tabMale[q];
+
+                    }
+                }
+                else if (checkBox3.Checked)
+                {
+                    m = rand.Next(0, tabZnakiSpecjalne.Length);
+                    haslo += tabZnakiSpecjalne[m];
+                    for (int j = 0; j < ileZnakow - 1; j++)
+                    {
+                        q = rand.Next(0, tabMale.Length);
+                        haslo += tabMale[q];
+
+                    }
+                }
+                else 
+                {
+                    for (int j = 0; j < ileZnakow; j++)
+                    {
+                        q = rand.Next(0, tabMale.Length);
+                        haslo += tabMale[q];
+
+                    }
+                }
+
+               
+
+               /*   if (checkBox1.Checked)
+                  {
+                      m = rand.Next(0, tabMaleDuze.Length);
+                      haslo += tabMaleDuze[m];
+                  }
+                  if (checkBox2.Checked)
+                  {
+                      m = rand.Next(0, tabCyfry.Length);
+                      haslo += tabCyfry[m];
+                  }
+                  if (checkBox3.Checked)
+                  {
+                      m = rand.Next(0, tabZnakiSpecjalne.Length);
+                      haslo += tabZnakiSpecjalne[m];
+
+                  
+            }
+                else
+                {
+                    m = rand.Next(0, tabZnakiSpecjalne.Length);
+                    haslo += tabMale[m];
+                }
+                //   haslo += tabMale[i];
+               */
+
+
+            
+            MessageBox.Show(haslo);
+
+        }
+    }
+}
